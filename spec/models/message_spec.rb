4785648a5cf912135bb7dbf3ef25f67d067e845b
felipeberger <feconchaber@gmail.com>
2022-04-25 15:38:54 +1100

    added basic message model tests

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Message, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "creates an invalid message" do
+    expect(Message.new).not_to be_valid
+  end
+
+  it "creates a valid message" do
+    expect(Message.new(content: "test")).to be_valid
+  end
+
 end
