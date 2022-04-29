c36c58deb9b3e6eea3b13cb2384917842efbbd73
felipeberger <feconchaber@gmail.com>
2022-04-29 16:33:22 +1100

    added a before section to test

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -2,16 +2,21 @@ require 'rails_helper'
 
 RSpec.describe Message, type: :model do
 
+  before do
+    @user = FactoryBot.create(:user)
+    @message = FactoryBot.create(:message, user: @user)
+  end
+
   it "creates an invalid message" do
     expect(Message.new).not_to be_valid
   end
 
   it "creates a valid message" do
-    expect(Message.new(content: "test")).to be_valid
+    expect(@message).to be_valid
   end
 
   describe 'associations' do
-    it { should belong_to(:user).without_validating_presence }
+    it { should belong_to(:user) }
   end
 
 end
