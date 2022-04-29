7d27fce642db0d31ea3496f639f80d604c9780af
felipeberger <feconchaber@gmail.com>
2022-04-29 19:09:13 +1100

    updated create method to use current_user

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -22,7 +22,7 @@ class MessagesController < ApplicationController
 
   # POST /messages or /messages.json
   def create
-    @message = Message.new(message_params)
+    @message = current_user.messages.new(message_params)
 
     respond_to do |format|
       if @message.save
