b393f00de2bdf5186ad279ab283bb23b6768e30e
felipeberger <feconchaber@gmail.com>
2022-04-29 16:31:44 +1100

    added reference to messages - they now belong to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_25_045552) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_29_052322) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_25_045552) do
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -26,4 +28,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_25_045552) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "messages", "users"
 end
